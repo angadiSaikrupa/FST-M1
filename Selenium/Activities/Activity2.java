a237f3c41a937e7605647e59bca03745ea877cd8
angadiSaikrupa <angadi.saikrupa@ibm.com>
2025-02-03 11:53:31 +0530

    Create Activity2.java

---
+++ Selenium/Activities/Activity2.java
@@ -0,0 +1,34 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity2 {
+
+	 public static void main(String[] args) {
+	        // Initialize the Firefox driver
+	        WebDriver driver = new FirefoxDriver();
+
+	        // Open the page
+	        driver.get("https://training-support.net/webelements/login-form");
+	        // Print the title of the page
+	        System.out.println("Page title: " + driver.getTitle());
+
+	        // Find the username field and enter the username
+	        driver.findElement(By.id("username")).sendKeys("admin");
+	        
+	        // Find the password field and enter the password
+	        driver.findElement(By.id("password")).sendKeys("password");
+	        
+	        // Find the submit button and click it
+	        driver.findElement(By.cssSelector("button.svelte-1pdjkmx")).click();
+	        
+
+	        // Print the confirmation message
+	        System.out.println( driver.getTitle());
+
+	        // Close the browser
+	        driver.quit();
+	    }
+	}
