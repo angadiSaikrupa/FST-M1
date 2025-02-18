3b00a6f288abd388ad71b98ba943013990d43cad
angadiSaikrupa <angadi.saikrupa@ibm.com>
2025-02-18 10:44:31 +0530

    Create LMS_Activity1.java

---
+++ Selenium/Project/LMS_Activity1.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebsiteTitleVerification {
+    public static void main(String[] args) {
+     
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+      
+        WebDriver driver = new ChromeDriver();
+
+        try {
+           
+            driver.get("https://alchemy.hguy.co/lms");
+
+          
+            String title = driver.getTitle();
+
+           
+            if ("Alchemy LMS – An LMS Application".equals(title)) {
+                System.out.println("Title matches successfully.");
+            } else {
+                System.out.println("Title does not match. The actual title is: " + title);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+          
+            driver.quit();
+        }
+    }
+}
